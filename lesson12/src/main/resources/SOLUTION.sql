f96307485dae39e0eb72e07ca7374b37fc79a3a9
lucciolla200 <teoteo.odisharia@gmail.com>
2023-03-05 00:14:56 +0400

    Module #3. Task #1. SQL v13

--- lesson12/src/main/resources/SOLUTION.sql
+++ lesson12/src/main/resources/SOLUTION.sql
@@ -1,4 +1,5 @@
-DELETE FROM student WHERE id in (select DISTINCT m.student_id FROM mark AS m INNER JOIN  Subject AS s ON m.subject_id = s.id WHERE s.grade >= 4);
-DELETE FROM student WHERE id in (select student_id FROM MARK GROUP BsY student_id, mark HAVING COUNT(mark) >= 1 AND mark < 4);
+DELETE FROM mark WHERE student_id IN (SELECT id FROM student WHERE groupnumber >= 4);
+DELETE FROM student WHERE groupnumber >= 4;
+DELETE FROM student WHERE id in (select student_id FROM mark GROUP BY student_id, mark HAVING COUNT(mark) >= 1 AND mark < 4);
 DELETE FROM paymenttype WHERE name = 'DAILY';
 DELETE FROM mark WHERE mark < 7;
